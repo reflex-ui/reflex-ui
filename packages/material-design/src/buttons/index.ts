b4525c3ad913c6b1a828d2e7f75dbcf0ff46884d
Flavio Silva <flsilva7@gmail.com>
2018-11-23 16:22:30 -0200

    refactor(renaming): rename packages/material-design/src/buttons/containedShaped/ to packages/material-design/src/buttons/contained-shaped/, and packages/material-design/src/buttons/outlinedShaped/ to packages/material-design/src/buttons/outlined-shaped/.

--- packages/material-design/src/buttons/index.ts
+++ packages/material-design/src/buttons/index.ts
@@ -1,11 +1,11 @@
 export * from './all-variants';
 export * from './contained';
-export * from './containedShaped';
+export * from './contained-shaped';
 export * from './default';
 export * from './fab';
 export * from './icon';
 export * from './outlined';
-export * from './outlinedShaped';
+export * from './outlined-shaped';
 export * from './raised';
 export * from './xfab';
 
