5b3a67628c25c0dae3aa0bf74c31bbdfbb6d92a5
Flavio Silva <flsilva7@gmail.com>
2018-12-21 19:39:52 -0200

    feat(Column): add new src/components/views/Column.tsx component.

---
+++ packages/core/src/components/views/Column.tsx
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) Flavio Silva https://flsilva.com
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import * as React from 'react';
+
+import { ColorTheme } from '../../palette/ColorTheme';
+import { ColorThemeContext } from '../../palette/ColorThemeContext';
+import { PaletteThemeContext } from '../../palette/PaletteThemeContext';
+import { DimensionsContext } from '../../responsiveness/DimensionsContext';
+// tslint:disable-next-line:max-line-length
+import { withResponsivenessProps } from '../../responsiveness/withResponsivenessProps';
+import { ComponentsThemeContext } from '../ComponentsThemeContext';
+import { reflexComponent } from '../reflexComponent';
+import { SimpleSuperView } from './SimpleSuperView';
+import { OptionalSuperViewProps, SuperViewProps } from './SuperViewProps';
+
+// prettier-ignore
+export const Column = withResponsivenessProps(
+  reflexComponent<OptionalSuperViewProps>({
+    name: 'Column',
+  })((props: OptionalSuperViewProps) => (
+    <DimensionsContext.Consumer>
+      {dimensionsProps => (
+        <ColorThemeContext.Consumer>
+          {ctxColorTheme => (
+            <PaletteThemeContext.Consumer>
+              {paletteTheme => (
+                <ComponentsThemeContext.Consumer>
+                  {(componentsTheme) => {
+                    const colorTheme: ColorTheme =
+                      props.colorTheme ||
+                      ctxColorTheme ||
+                      ColorTheme.SURFACE_NORMAL;
+
+                    const propsWithDefaults: SuperViewProps = {
+                      ...dimensionsProps,
+                      colorTheme,
+                      paletteTheme,
+                      theme: componentsTheme.views.column,
+                      ...props,
+                    };
+
+                    return (
+                      <ColorThemeContext.Provider value={colorTheme}>
+                        <SimpleSuperView {...propsWithDefaults} />
+                      </ColorThemeContext.Provider>
+                    );
+                  }}
+                </ComponentsThemeContext.Consumer>
+              )}
+            </PaletteThemeContext.Consumer>
+          )}
+        </ColorThemeContext.Consumer>
+      )}
+    </DimensionsContext.Consumer>
+  )),
+);
