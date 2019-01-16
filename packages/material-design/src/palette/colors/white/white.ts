39c0070d1e8105ab8c9d53f2a50829070fadbe7c
Flavio Silva <flsilva7@gmail.com>
2019-01-16 18:53:10 -0200

    fix(md:white) fix interaction colors of uncontained white md color.

--- packages/material-design/src/palette/colors/white/white.ts
+++ packages/material-design/src/palette/colors/white/white.ts
@@ -8,6 +8,7 @@
 import {
   createPaletteColorInteraction,
   fromOnColorOnlyPaletteColor,
+  InteractivityPaletteColor,
   invertPaletteColor,
   PaletteColor,
 } from '@reflex-ui/core';
@@ -25,6 +26,7 @@ import { disabledGrey500 } from '../disabled/uncontained/disabledGrey500';
 // tslint:disable-next-line:max-line-length
 import { disabledGrey600_500_600 } from '../disabled/uncontained/disabledGrey600_500_600';
 import { MaterialPaletteColorItem } from '../MaterialPaletteColorItem';
+import { transparent } from '../transparent';
 
 /*
  * This is the simplest way (for this specific case) to avoid
@@ -51,18 +53,27 @@ const regularContainedBaseColor: PaletteColor = {
   },
 };
 
-const regularUncontainedBaseColor: PaletteColor = {
-  dark: {
-    color: white,
-    onColor: white,
+const whiteRegularUncontainedColorInteraction: InteractivityPaletteColor = {
+  disabled: disabledGrey500,
+  enabled: {
+    dark: { color: transparent, onColor: white },
+    light: { color: transparent, onColor: white },
+    normal: { color: transparent, onColor: white },
   },
-  light: {
-    color: white,
-    onColor: white,
+  focused: {
+    dark: { color: '#e0e0e0', onColor: white },
+    light: { color: '#e0e0e0', onColor: white },
+    normal: { color: '#e0e0e0', onColor: white },
   },
-  normal: {
-    color: white,
-    onColor: white,
+  hovered: {
+    dark: { color: '#f5f5f5', onColor: white },
+    light: { color: '#f5f5f5', onColor: white },
+    normal: { color: '#f5f5f5', onColor: white },
+  },
+  pressed: {
+    dark: { color: '#d6d6d6', onColor: white },
+    light: { color: '#d6d6d6', onColor: white },
+    normal: { color: '#d6d6d6', onColor: white },
   },
 };
 
@@ -86,11 +97,7 @@ export const mdWhite: MaterialPaletteColorItem = {
         colorVariantFactory: createContainedPaletteColorVariant,
         disabledColor: disabledGrey300_500,
       }),
-      uncontained: createPaletteColorInteraction({
-        baseColor: regularUncontainedBaseColor,
-        colorVariantFactory: createUncontainedPaletteColorVariant,
-        disabledColor: disabledGrey500,
-      }),
+      uncontained: whiteRegularUncontainedColorInteraction,
     },
   },
   name: 'White',
