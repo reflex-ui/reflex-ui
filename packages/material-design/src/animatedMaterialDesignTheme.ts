ab1d040148d213be3e2ab2702d0e03c5ae104756
Flavio Silva <flsilva7@gmail.com>
2019-01-17 11:02:41 -0200

    fix(md: animated theme) fix animated theme to include recently created components (AppBar, Surface, and Views).

--- packages/material-design/src/animatedMaterialDesignTheme.ts
+++ packages/material-design/src/animatedMaterialDesignTheme.ts
@@ -7,12 +7,18 @@
 
 import { OptionalComponentsTheme } from '@reflex-ui/core';
 
+import { appBarTheme } from './app-bars/appBarTheme';
 import { animatedButtonTheme } from './buttons/animatedTheme';
 import { iconTheme } from './icons/iconTheme';
+import { surfaceTheme } from './surface/theme';
 import { typographyTheme } from './typography/typographyTheme';
+import { superViewTheme } from './views/theme';
 
 export const animatedMaterialDesignTheme: OptionalComponentsTheme = {
+  appBar: appBarTheme,
   button: animatedButtonTheme,
   icon: iconTheme,
+  surface: surfaceTheme,
   typography: typographyTheme,
+  views: superViewTheme,
 };
