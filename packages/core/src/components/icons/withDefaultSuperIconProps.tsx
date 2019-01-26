1ee7e9125156a06b0f6a26ad8e8e506f033bb65d
Flavio Silva <flsilva7@gmail.com>
2019-01-26 21:41:20 -0200

    fix(core/icon) fix withDefaultSuperIconProps to consider ColorThemeContext color value.

--- packages/core/src/components/icons/withDefaultSuperIconProps.tsx
+++ packages/core/src/components/icons/withDefaultSuperIconProps.tsx
@@ -7,7 +7,9 @@
 
 import * as React from 'react';
 
-import { ColorTheme, PaletteThemeContext } from '../../palette';
+import { ColorTheme } from '../../palette/ColorTheme';
+import { ColorThemeContext } from '../../palette/ColorThemeContext';
+import { PaletteThemeContext } from '../../palette/PaletteThemeContext';
 import { Size } from '../../sizing/Size';
 import { ComponentsThemeContext } from '../ComponentsThemeContext';
 import { reflexComponent } from '../reflexComponent';
@@ -21,19 +23,23 @@ OptionalSuperIconProps
 >({ name: 'WithDefaultSuperIconProps', wrapped: WrappedComponent })(props => (
   <PaletteThemeContext.Consumer>
     {paletteTheme => (
-      <ComponentsThemeContext.Consumer>
-        {(componentsTheme) => {
-          const propsWithDefaults: SuperIconProps = {
-            colorTheme: ColorTheme.SecondaryNormal,
-            paletteTheme,
-            size: Size.M,
-            theme: componentsTheme.icon,
-            ...props,
-          };
+      <ColorThemeContext.Consumer>
+        {colorTheme => (
+          <ComponentsThemeContext.Consumer>
+            {(componentsTheme) => {
+              const propsWithDefaults: SuperIconProps = {
+                colorTheme: colorTheme || ColorTheme.SecondaryNormal,
+                paletteTheme,
+                size: Size.M,
+                theme: componentsTheme.icon,
+                ...props,
+              };
 
-          return <WrappedComponent {...propsWithDefaults} />;
-        }}
-      </ComponentsThemeContext.Consumer>
+              return <WrappedComponent {...propsWithDefaults} />;
+            }}
+          </ComponentsThemeContext.Consumer>
+        )}
+      </ColorThemeContext.Consumer>
     )}
   </PaletteThemeContext.Consumer>
 ));
