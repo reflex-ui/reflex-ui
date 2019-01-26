1ca9d09059073caf0b14b83caad3c0adffbb73de
Flavio Silva <flsilva7@gmail.com>
2019-01-26 21:44:12 -0200

    feat(core/typography) add remaining typography components.

---
+++ packages/core/src/components/typography/Paragraph1.tsx
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) Flavio Silva https://flsilva.com
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import * as React from 'react';
+
+import { ColorTheme } from '../../palette/ColorTheme';
+import { ColorThemeContext } from '../../palette/ColorThemeContext';
+import { PaletteThemeContext } from '../../palette/PaletteThemeContext';
+import { ComponentsThemeContext } from '../ComponentsThemeContext';
+import { reflexComponent } from '../reflexComponent';
+import { SimpleText } from './SimpleText';
+import { OptionalTypographyProps, TypographyProps } from './TypographyProps';
+
+// prettier-ignore
+export const Paragraph1 = reflexComponent<OptionalTypographyProps>({
+  name: 'Paragraph1',
+})((props: OptionalTypographyProps) => (
+  <PaletteThemeContext.Consumer>
+    {paletteTheme => (
+      <ColorThemeContext.Consumer>
+        {colorTheme => (
+          <ComponentsThemeContext.Consumer>
+            {(componentsTheme) => {
+              const propsWithDefaults: TypographyProps = {
+                colorTheme: props.colorTheme || colorTheme ||
+                  ColorTheme.SurfaceNormal,
+                paletteTheme,
+                theme: componentsTheme.typography.paragraph1,
+                ...props,
+              };
+
+              return <SimpleText {...propsWithDefaults} />;
+            }}
+          </ComponentsThemeContext.Consumer>
+        )}
+      </ColorThemeContext.Consumer>
+    )}
+  </PaletteThemeContext.Consumer>
+));
