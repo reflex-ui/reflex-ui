7faac3f7a870577e1c740d98ada05a4d232ae037
Flavio Silva <flsilva7@gmail.com>
2019-01-26 16:47:33 -0200

    feat(showcase/list) use new <TouchableSurface> component.

--- showcase/cross-platform-ts/src/screens/list/ListShowcaseScreen.tsx
+++ showcase/cross-platform-ts/src/screens/list/ListShowcaseScreen.tsx
@@ -5,9 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { Headline6, List, ListItem } from '@reflex-ui/core';
+import { Headline6, List, ListItem, TouchableSurface } from '@reflex-ui/core';
 import * as React from 'react';
 import { ScrollView } from 'react-native';
+
+const onListItemPress = () => {
+  // tslint:disable-next-line:no-console
+  console.log('ListShowcaseScreen().onListItemPress()');
+};
 
 const ListShowcaseScreen: React.SFC = (): JSX.Element => (
   <ScrollView>
@@ -15,9 +20,11 @@ const ListShowcaseScreen: React.SFC = (): JSX.Element => (
       <ListItem>
         <Headline6>Inbox</Headline6>
       </ListItem>
-      <ListItem>
-        <Headline6>Starred</Headline6>
-      </ListItem>
+      <TouchableSurface onPress={onListItemPress}>
+        <ListItem>
+          <Headline6>Starred</Headline6>
+        </ListItem>
+      </TouchableSurface>
       <ListItem>
         <Headline6>Snoozed</Headline6>
       </ListItem>
