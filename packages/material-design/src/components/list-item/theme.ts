1d5e934e0533d675501356e4485b8e8e0063702c
Flavio Silva <flsilva7@gmail.com>
2019-01-30 23:23:41 -0200

    feat(md/list-item) improve styles to handle sizing.

--- packages/material-design/src/components/list-item/theme.ts
+++ packages/material-design/src/components/list-item/theme.ts
@@ -10,17 +10,36 @@ import {
   ListItemTheme,
   OptionalListItemTheme,
   rawListItemTheme,
+  Size,
   ViewStyleGetter,
 } from '@reflex-ui/core';
 import merge from 'lodash/merge';
+import { ViewStyle } from 'react-native';
 
-export const getListItemContainerStyle: ViewStyleGetter<
-  ListItemProps
-> = () => ({
+export const listItemContainerSizedStyle: { [key in Size]: ViewStyle } = {
+  xxsmall: { minHeight: 32 },
+  /*
+   * Sorting values by size here makes it easier to reason about
+   * the overall scale of values than sorting alphabetically,
+   * so let's just disable this rule here.
+   */
+  // tslint:disable-next-line:object-literal-sort-keys
+  xsmall: { minHeight: 40 },
+  small: { minHeight: 48 },
+  medium: { minHeight: 56 },
+  large: { minHeight: 64 },
+  xlarge: { minHeight: 72 },
+  xxlarge: { minHeight: 88 },
+};
+
+export const getListItemContainerStyle: ViewStyleGetter<ListItemProps> = ({
+  size,
+}) => ({
+  ...listItemContainerSizedStyle[size],
   alignItems: 'center',
   backgroundColor: 'transparent',
   flexDirection: 'row',
-  minHeight: 48,
+  paddingHorizontal: 8,
 });
 
 export const partialListItemTheme: OptionalListItemTheme = {
