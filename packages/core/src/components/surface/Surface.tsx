d064164b0dd20c337a8bd7fb9337624d0d5ba9e6
Flavio Silva <flsilva7@gmail.com>
2019-03-25 20:23:40 -0300

    refactor(Surface) make it an specialized RfxView like Column and Row

--- packages/core/src/components/surface/Surface.tsx
+++ packages/core/src/components/surface/Surface.tsx
@@ -6,77 +6,16 @@
  */
 
 import * as React from 'react';
-import { View, ViewProps } from 'react-native';
 
 import { ColorThemeContext } from '../../palette/ColorThemeContext';
 import { useOnLayout } from '../../responsiveness/useOnLayout';
-import { getStyleFromTheme } from '../getStyleFromTheme';
 import { handleChildrenProps } from '../handleChildrenProps';
 import { handlePatchThemeProps } from '../handlePatchThemeProps';
 import { processComponent } from '../processComponent';
 import { validateNoStyleProps } from '../validateNoStyleProps';
-import { SurfaceProps, SurfacePropsOptional } from './SurfaceProps';
+import { renderRfxViewContainer } from '../view/RfxView';
+import { SurfacePropsOptional } from './SurfaceProps';
 import { useDefaultSurfaceProps } from './useDefaultSurfaceProps';
-
-export const extractViewPropsFromSurfaceProps = (
-  props: SurfaceProps,
-): ViewProps => {
-  const {
-    alignContent,
-    alignItems,
-    alignSelf,
-    children,
-    colorTheme,
-    flex,
-    flexBasis,
-    flexDirection,
-    flexGrow,
-    flexShrink,
-    flexWrap,
-    getPatchTheme,
-    justifyContent,
-    margin,
-    marginBottom,
-    marginEnd,
-    marginHorizontal,
-    marginStart,
-    marginTop,
-    marginVertical,
-    padding,
-    paddingBottom,
-    paddingEnd,
-    paddingHorizontal,
-    paddingStart,
-    paddingTop,
-    paddingVertical,
-    paletteTheme,
-    theme,
-    ...viewProps
-  } = props;
-
-  return viewProps;
-};
-
-export const renderSurfaceView = (props: SurfaceProps): JSX.Element => {
-  const { children, onLayout, theme } = props;
-
-  const Container = theme.component || View;
-  const viewProps = {
-    ...extractViewPropsFromSurfaceProps(props),
-    onLayout,
-    style: getStyleFromTheme(props, theme),
-  };
-
-  if (Container === View) {
-    return <Container {...viewProps}>{children}</Container>;
-  }
-
-  return (
-    <Container complexComponentProps={props} {...viewProps}>
-      {children}
-    </Container>
-  );
-};
 
 let Surface: React.ComponentType<SurfacePropsOptional> = (
   props: SurfacePropsOptional,
@@ -89,7 +28,7 @@ let Surface: React.ComponentType<SurfacePropsOptional> = (
 
   return (
     <ColorThemeContext.Provider value={newProps.colorTheme}>
-      {renderSurfaceView(newProps)}
+      {renderRfxViewContainer(newProps)}
     </ColorThemeContext.Provider>
   );
 };
--- packages/core/src/components/surface/SurfaceProps.ts
+++ packages/core/src/components/surface/SurfaceProps.ts
@@ -5,27 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { ViewProps } from 'react-native';
-
-import { FlexboxProps } from '../../flexbox/FlexboxProps';
-import { PaletteThemeProps } from '../../palette/PaletteThemeProps';
-import { DimensionsProps } from '../../responsiveness/DimensionsProps';
-import { ResponsivenessProps } from '../../responsiveness/ResponsivenessProps';
-import { MarginProps } from '../../spacing/MarginProps';
-import { PaddingProps } from '../../spacing/PaddingProps';
 import { ComponentChildrenProps } from '../ComponentChildrenProps';
 import { ComponentThemeProps } from '../ComponentThemeProps';
+import { RfxViewProps } from '../view/RfxViewProps';
 import { SurfaceTheme } from './SurfaceTheme';
 
 export interface SurfaceProps
   extends ComponentChildrenProps<SurfaceProps>,
     ComponentThemeProps<SurfaceProps, SurfaceTheme>,
-    DimensionsProps,
-    FlexboxProps,
-    MarginProps,
-    PaddingProps,
-    PaletteThemeProps,
-    ResponsivenessProps,
-    ViewProps {}
+    RfxViewProps {}
 
 export type SurfacePropsOptional = Partial<SurfaceProps>;
