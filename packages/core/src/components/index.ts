aaa0819fb60bddf31d40f1e3b47476876d845ddc
Flavio Silva <flsilva7@gmail.com>
2019-04-05 14:41:27 -0300

    refactor(cleanup) delete handlePatchThemeProps, not used anymore

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -15,7 +15,6 @@ export * from './ComponentThemeGetter';
 export * from './ComponentThemeProps';
 export * from './getPropsFromTheme';
 export * from './getStyleFromTheme';
-export * from './handlePatchThemeProps';
 export * from './isValidStyle';
 export * from './list';
 export * from './list-item';
