8bc72193e130dfe4cf2e437ada0a357b73e65de9
Flavio Silva <flsilva7@gmail.com>
2019-05-09 18:30:28 -0300

    feat(showcase/view) add <AppBar> to view screen

--- apps/core-showcase/src/screens/view/ViewScreen.tsx
+++ apps/core-showcase/src/screens/view/ViewScreen.tsx
@@ -6,14 +6,26 @@
  */
 
 import {
+  AppBar,
+  AppBarTitle,
+  Button,
+  ButtonVariant,
   Column,
   Row,
+  Screen,
   Size,
   SpaceBetween,
   Surface,
   usePalette,
 } from '@reflex-ui/core';
+import { MenuIcon } from '@reflex-ui/icons-md';
 import React from 'react';
+import { ScrollView } from 'react-native';
+
+const onButtonPress = () => {
+  // tslint:disable-next-line:no-console
+  console.log('ViewScreen().onButtonPress()');
+};
 
 const Box = () => (
   <Surface
@@ -25,37 +37,47 @@ const Box = () => (
 );
 
 const ViewScreen: React.SFC<{}> = (): JSX.Element => (
-  <Column>
-    <Row>
-      <Box />
-      <Box />
-    </Row>
-    <SpaceBetween>
-      <Box />
-      <Box />
-    </SpaceBetween>
-    <SpaceBetween maxWidth={300}>
-      <Box />
-      <Box />
-    </SpaceBetween>
-    <SpaceBetween maxWidth={300}>
-      <Row>
-        <Box />
-        <Box />
-      </Row>
-      <Box />
-    </SpaceBetween>
-    <SpaceBetween maxWidth={300}>
-      <Row>
-        <Box />
-        <Box />
-      </Row>
-      <Row>
-        <Box />
-        <Box />
-      </Row>
-    </SpaceBetween>
-  </Column>
+  <Screen>
+    <AppBar>
+      <Button onPress={onButtonPress} variant={ButtonVariant.Icon}>
+        <MenuIcon />
+      </Button>
+      <AppBarTitle numberOfLines={1}>View</AppBarTitle>
+    </AppBar>
+    <ScrollView>
+      <Column margin={Size.S} marginTop={Size.XXL}>
+        <Row>
+          <Box />
+          <Box />
+        </Row>
+        <SpaceBetween>
+          <Box />
+          <Box />
+        </SpaceBetween>
+        <SpaceBetween maxWidth={300}>
+          <Box />
+          <Box />
+        </SpaceBetween>
+        <SpaceBetween maxWidth={300}>
+          <Row>
+            <Box />
+            <Box />
+          </Row>
+          <Box />
+        </SpaceBetween>
+        <SpaceBetween maxWidth={300}>
+          <Row>
+            <Box />
+            <Box />
+          </Row>
+          <Row>
+            <Box />
+            <Box />
+          </Row>
+        </SpaceBetween>
+      </Column>
+    </ScrollView>
+  </Screen>
 );
 
 ViewScreen.displayName = 'ViewScreen';
