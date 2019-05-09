53e39419bf7dfd032f8a8c8a38a94948e5480670
Flavio Silva <flsilva7@gmail.com>
2019-05-09 17:52:48 -0300

    feat(view) implement new <SpaceBetween> component

---
+++ packages/core/src/components/view/SpaceBetween.tsx
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) Flavio Silva https://flsilva.com
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { forwardRef, Ref } from 'react';
+import { View } from 'react-native';
+
+import { MissingComponentThemeError } from '../../errors';
+import { useOnLayout } from '../../responsiveness/useOnLayout';
+import { useComponentsTheme } from '../ComponentsTheme';
+import { processComponent } from '../processComponent';
+import { processComponentProps } from '../processComponentProps';
+import { processThemeAndStyleProps } from '../processThemeAndStyleProps';
+import { renderRfxViewComponent } from './renderRfxViewComponent';
+import { RfxViewProps, RfxViewPropsOptional } from './RfxViewProps';
+import { RfxViewTheme } from './RfxViewTheme';
+import { useDefaultRfxViewProps } from './useDefaultRfxViewProps';
+import { useShouldProvideColor } from './useShouldProvideColor';
+
+const useTheme = (theme?: RfxViewTheme): RfxViewTheme => {
+  const { componentsTheme } = useComponentsTheme();
+
+  if (theme !== undefined && theme !== null) return theme;
+  if (componentsTheme.views === undefined || componentsTheme.views === null) {
+    throw new MissingComponentThemeError('<SpaceBetween>');
+  }
+
+  return componentsTheme.views.spaceBetween;
+};
+
+let SpaceBetween: React.ComponentType<RfxViewPropsOptional> = forwardRef(
+  (props: RfxViewPropsOptional, ref: Ref<View>) => {
+    const theme = useTheme(props.theme);
+
+    let newProps: RfxViewProps = useDefaultRfxViewProps(props, theme);
+    newProps = { ...newProps, ...useOnLayout(newProps) };
+    newProps = processComponentProps(newProps);
+    newProps = processThemeAndStyleProps(newProps, newProps.theme);
+
+    const shouldProvideColor = useShouldProvideColor(props.paletteColor);
+    return renderRfxViewComponent({ props: newProps, shouldProvideColor, ref });
+  },
+);
+
+SpaceBetween = processComponent<RfxViewPropsOptional>(SpaceBetween, {
+  name: 'SpaceBetween',
+});
+
+export { SpaceBetween };
